9c2e3bb096fb6478c533b68d8552caeb55bf5268
Jeffrey Rose <jtr219@outlook.com>
2020-05-12 00:34:09 -0400

    built function to take gridString and confirm valid, then solve + testing suite

---
+++ src/solvers/checkAndSolve.js
@@ -0,0 +1,19 @@
+import { checkValid } from "./checkValid";
+import solveWithStandardOptions from "./applyStratsWithChains";
+
+const checkAndSolve = gridString => {
+    const confirmValid = checkValid(gridString);
+    if (!confirmValid.valid) {
+        return confirmValid;
+    } else {
+        const attempt = solveWithStandardOptions(confirmValid.formattedGrid);
+        const strategiesUsed = attempt.solutions.map(x => x.strategy);
+        return {
+            ...attempt,
+            strategiesUsed,
+            valid: true
+        };
+    }
+}
+
+export default checkAndSolve;
